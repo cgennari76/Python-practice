cb4a7066017a144f65fd8dbd56d5be6e2fa488a4
cgennari76 <31255261+cgennari76@users.noreply.github.com>
2021-10-31 12:04:08 -0700

    changed class vars to instance vars

--- jobs.py
+++ jobs.py
@@ -6,14 +6,15 @@ class Person(object):
         self.quote = quote
 
 class Display:
-    personLst = []
-    personLst.append(Person("Payne N. Diaz", "coach", "Without exception, there is no rule!"))
-    personLst.append(Person("Mia Serts", "bicyclist", "If the world didn't suck, we'd all fall off!"))
-    personLst.append(Person("Don B. Sanosi", "teacher", "Work real hard while you wait and good things will come to you!"))
-    personLst.append(Person("Hugh Jorgan", "organist", "Age is a very high price to pay for maturity."))
-    personLst.append(Person("Herasmus B. Dragon", "dentist", "Enough people can't find work in America!"))
-    personLst.append(Person("Adolph Koors", "master-brewer", "Wish you were beer!"))
-    personLst.append(Person("Zucker Zahn", "dentist", "If you drink from the fountain of knowledge, quench your thirst slowly."))
+    def plist():
+        d.personLst = []
+        d.personLst.append(Person("Payne N. Diaz", "coach", "Without exception, there is no rule!"))
+        d.personLst.append(Person("Mia Serts", "bicyclist", "If the world didn't suck, we'd all fall off!"))
+        d.personLst.append(Person("Don B. Sanosi", "teacher", "Work real hard while you wait and good things will come to you!"))
+        d.personLst.append(Person("Hugh Jorgan", "organist", "Age is a very high price to pay for maturity."))
+        d.personLst.append(Person("Herasmus B. Dragon", "dentist", "Enough people can't find work in America!"))
+        d.personLst.append(Person("Adolph Koors", "master-brewer", "Wish you were beer!"))
+        d.personLst.append(Person("Zucker Zahn", "dentist", "If you drink from the fountain of knowledge, quench your thirst slowly."))
 
     def oneitem():
         print("Show one particular item:")
@@ -30,6 +31,7 @@ class Display:
         
     def zonedesk():
         d = Display
+        d.plist()
         d.oneitem()
         d.allitem()
 
