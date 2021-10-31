978b1b795ff9e588cac18ec0c780b4ca587ae1c1
cgennari76 <31255261+cgennari76@users.noreply.github.com>
2021-10-31 12:01:24 -0700

    replaced method with class variables

--- jobs.py
+++ jobs.py
@@ -6,28 +6,26 @@ class Person(object):
         self.quote = quote
 
 class Display:
-# make a list of class Person(s)
-    def plist(self):
-        personList = []
-        personList.append(Person("Payne N. Diaz", "coach", "Without exception, there is no rule!"))
-        personList.append(Person("Mia Serts", "bicyclist", "If the world didn't suck, we'd all fall off!"))
-        personList.append(Person("Don B. Sanosi", "teacher", "Work real hard while you wait and good things will come to you!"))
-        personList.append(Person("Hugh Jorgan", "organist", "Age is a very high price to pay for maturity."))
-        personList.append(Person("Herasmus B. Dragon", "dentist", "Enough people can't find work in America!"))
-        personList.append(Person("Adolph Koors", "master-brewer", "Wish you were beer!"))
-        personList.append(Person("Zucker Zahn", "dentist", "If you drink from the fountain of knowledge, quench your thirst slowly."))
+    personLst = []
+    personLst.append(Person("Payne N. Diaz", "coach", "Without exception, there is no rule!"))
+    personLst.append(Person("Mia Serts", "bicyclist", "If the world didn't suck, we'd all fall off!"))
+    personLst.append(Person("Don B. Sanosi", "teacher", "Work real hard while you wait and good things will come to you!"))
+    personLst.append(Person("Hugh Jorgan", "organist", "Age is a very high price to pay for maturity."))
+    personLst.append(Person("Herasmus B. Dragon", "dentist", "Enough people can't find work in America!"))
+    personLst.append(Person("Adolph Koors", "master-brewer", "Wish you were beer!"))
+    personLst.append(Person("Zucker Zahn", "dentist", "If you drink from the fountain of knowledge, quench your thirst slowly."))
 
     def oneitem():
         print("Show one particular item:")
-        print(personList[0].name)
+        print(d.personLst[0].name)
         
     def allitem():
-        print("Sort the personList in place by job ...")
+        print("Sort the personLst in place by job ...")
         import operator
-        personList.sort(key=operator.attrgetter('job'))
+        d.personLst.sort(key=operator.attrgetter('job'))
 
         print("... then show all quotes and who said so:")
-        for person in personList:
+        for person in d.personLst:
             print("\"%s\"  %s (%s)" % (person.quote, person.name, person.job))
         
     def zonedesk():
